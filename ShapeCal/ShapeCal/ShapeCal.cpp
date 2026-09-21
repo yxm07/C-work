22a51bd42f03efc8fafbfd229eea2958815aeccf
yxm07 <3031969646@qq.com>
2026-09-21 23:50:40 +0800

    TriangleItem题目类

--- ShapeCal/ShapeCal/ShapeCal.cpp
+++ ShapeCal/ShapeCal/ShapeCal.cpp
@@ -1,20 +1,81 @@
-﻿// ShapeCal.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+class TriangleItem {
+private:
+    int m_sideA;
+    int m_sideB;
+    int m_sideC;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+public:
+    //初始化 (无参)
+    TriangleItem() {
+        m_sideA = 0; m_sideB = 0; m_sideC = 0;
+    }
+
+    //初始化 (带参)
+    TriangleItem(int a, int b, int c) {
+        m_sideA = a; m_sideB = b; m_sideC = c;
+    }
+
+    //修改
+    void set(int a, int b, int c) {
+        m_sideA = a; m_sideB = b; m_sideC = c;
+    }
+
+    //获取
+    int getSideA() { return m_sideA; }
+    int getSideB() { return m_sideB; }
+    int getSideC() { return m_sideC; }
+
+    //输出
+    void printTri() {
+        cout << "三角形的三边分别为: " << m_sideA << ", " << m_sideB << ", " << m_sideC << endl;
+    }
+
+    //判断是否为三角形
+    bool isTriangle() {
+        return (m_sideA + m_sideB > m_sideC && m_sideA + m_sideC > m_sideB && m_sideB + m_sideC > m_sideA);
+    }
+
+    //计算周长
+    int calPerimeter() {
+        if (!isTriangle()) return 0;
+        return m_sideA + m_sideB + m_sideC;
+    }
+
+    //计算面积
+    float calArea() {
+        if (!isTriangle()) return 0.0;
+        float p = (m_sideA + m_sideB + m_sideC) / 2.0;//半周长
+        return sqrt(p * (p - m_sideA) * (p - m_sideB) * (p - m_sideC));//海伦公式
+    }
+
+    //判断类型
+    void printType() {
+        if (!isTriangle()) {
+            cout << "不是三角形" << endl;
+            return;
+        }
+        if (m_sideA == m_sideB && m_sideB == m_sideC) {
+            cout << "等边三角形" << endl;
+        }
+        else if (m_sideA == m_sideB || m_sideA == m_sideC || m_sideB == m_sideC) {
+            cout << "等腰三角形" << endl;
+        }
+        else {
+            cout << "普通三角形" << endl;
+        }
+    }
+};
+
+int main() {
+    TriangleItem t(3, 4, 5);
+    t.printTri();
+    t.printType();
+    cout << "周长为: " << t.calPerimeter() << endl;
+    cout << "面积为: " << t.calArea() << endl;
+
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
